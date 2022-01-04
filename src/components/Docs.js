20107a3fd606fabfb3f7c7505c508886cca7c2dc
rgdonovan <55225918+rgdonovan@users.noreply.github.com>
2022-01-04 20:12:55 +0100

    Update Docs.js
    Co-authored-by: Sam Clark <63471713+clarksam19@users.noreply.github.com>
    Co-authored-by: Justin Lo <50888159+justinnnlo@users.noreply.github.com>
    Co-authored-by: Rana Deeb <62123706+ranadeeb92@users.noreply.github.com>

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -8,7 +8,7 @@ const Docs = () => {
   return (
     <div className="flex flex-col text-raisin text-xl h-screen">
       <header className="sticky align-middle top-0 bg-white flex py-1.5 pl-2 items-center border-b border-gray-200">
-        <img src={icon} alt="The lodge icon" className="w-14 p-2" />
+        <Link to="/"><img src={icon} alt="The lodge icon" className="w-14 p-2" /></Link>
         <h4 className="font-bold text-lg ml-4 mt-4 self-center">Lodge Overview</h4>
       </header>
       <div className="flex flex-col md:flex-row">
@@ -17,10 +17,6 @@ const Docs = () => {
             <li className="m-2 p-1.5 pl-6 rounded bg-gray-100 hover:text-cyan">
               <a href="#">Introduction</a>
             </li>
-            <li className="m-2 p-1.5 pl-6 rounded bg-gray-100">
-              <a href="#">Expandable</a>
-            </li>
-
           </ul>
         </nav>
         <div className="flex flex-col items-center">
@@ -29,7 +25,7 @@ const Docs = () => {
             <div>
               <h1 className="text-4xl font-bold my-8">Overview</h1>
               <p className="leading-9">Tapestry is an open source orchestration framework for the deployment of user entity data pipelines. It allows users to easily configure and launch an end-to-end data pipeline hosted on Amazon Web Services. Our automated solution combines best-in-class tools to create a warehouse-centric data stack, offering built-in data ingestion, transformation, and newly emerging data syncing (also known as "reverse ETL") technologies. Our inclusion of a reverse ETL component solves the "last mile" problem by providing the ability to operationalize collected user data in near real time.</p>
-              <Link to="/#case-study" className="block my-8 text-cyan">Read our case study for more information about user data pipelines and to learn how we built Lodge.</Link>
+              <Link to="/" className="block my-8 text-cyan">Read our case study for more information about user data pipelines and to learn how we built Lodge.</Link>
             </div>
             <div className="text-right border border-gray-300 rounded-lg p-4 w-56 self-end">
               <p className="text-md mb-1">Next</p>
@@ -41,7 +37,7 @@ const Docs = () => {
       <footer className="bg-raisin px-6 md:px-24 text-white h-96">
         <h6 className="font-bold my-8">Lodge Resources</h6>
         <a className="block my-2">Github</a>
-        <Link to="/#landing"><p className="block pb-8">Case Study</p></Link>
+        <Link to="/"><p className="block pb-8">Case Study</p></Link>
       </footer>
     </div>
 
