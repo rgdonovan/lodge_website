589a237a7f07ad1402414ae0148d41f4b9081c99
rgdonovan <55225918+rgdonovan@users.noreply.github.com>
2022-01-04 20:09:53 +0100

    Update Nav.js
    Co-authored-by: Sam Clark <63471713+clarksam19@users.noreply.github.com>
    Co-authored-by: Justin Lo <50888159+justinnnlo@users.noreply.github.com>
    Co-authored-by: Rana Deeb <62123706+ranadeeb92@users.noreply.github.com>

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,24 +13,24 @@ function Nav() {
           <img src={logo} alt="the Lodge logo" className="h-20 ml-8" />
         </a>
         <nav className="hidden lg:flex bg-white h-full justify-between items-center text-center text-lg text-gray-500 font-medium">
-          <a href="#landing" className="mx-4">Start Here</a>
-          <a href="#case-study" className="mx-4">Case Study</a>
-          <a href="#presentation" className="mx-4">Presentation</a>
-          <a href="#our-team" className="mx-4">Our Team</a>
+          <a href="/#landing" className="mx-4">Start Here</a>
+          <a href="/#case-study" className="mx-4">Case Study</a>
+          <a href="/#presentation" className="mx-4">Presentation</a>
+          <a href="/#our-team" className="mx-4">Our Team</a>
           <a href="/docs" className="mx-4">Docs</a>
-          <a href="https://github.com/lodge-cli" className="mx-4 w-20"><img src={icon} className="w-full h-10 " alt="The github logo" /></a>
+          <a href="https://github.com/lodge-logging/Lodge" className="mx-4 w-20"><img src={icon} className="w-full h-10" alt="The github logo" /></a>
         </nav>
         <div className="lg:hidden block">
           <button type="button" className={`${showMenu ? "bg-red-500" : "bg-green-300"} w-8 p-8 m-4`} onClick={() => setShowMenu(!showMenu)} />
         </div>
       </div>
       <div id="mobile-menu" className={`lg:hidden ${showMenu ? "translate-y-20" : "-translate-y-full"} z-0 bg-white fixed flex flex-col text-xl text-gray-800 w-full gap-1 transform transition duration-500 ease-in-out`}>
-        <a href="#start-here" className={`${navItemStyle} mt-1`}>Start Here</a>
-        <a href="#case-study" className={navItemStyle}>Case Study</a>
-        <a href="#presentation" className={navItemStyle}>Presentation</a>
-        <a href="#our-team" className={navItemStyle}>Our Team</a>
-        <a href="https://carnelian-storm-690.notion.site/Using-Ekko-80ae32174bb24c05b16ec4915715deab" className={navItemStyle}>Docs</a>
-        <a href="https://github.com/ekko-realtime" className={`${navItemStyle} mb-1`}><i className="fab fa-github"></i> GitHub</a>
+        <a href="/#landing" className={`${navItemStyle} mt-1`}>Start Here</a>
+        <a href="/#case-study" className={navItemStyle}>Case Study</a>
+        <a href="/#presentation" className={navItemStyle}>Presentation</a>
+        <a href="/#our-team" className={navItemStyle}>Our Team</a>
+        <a href="/docs" className={navItemStyle}>Docs</a>
+        <a href="https://github.com/lodge-logging/Lodge" className={`${navItemStyle} mb-1`}><i className=""></i> GitHub</a>
       </div>
     </header>
   );
